0bd6b652a5eab9450cfa56ba6fa7afeb77cfb733
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-03-01 10:38:11 +0300

    updatd functions

--- Lab 5 - JS Fundamentals/Tutorials/tutorial.js
+++ Lab 5 - JS Fundamentals/Tutorials/tutorial.js
@@ -27,7 +27,7 @@ y = 50;
 x = 50;
 console.log("Outside z =", y);
 
-*/
+
 
 const person = {
   name: "Ali",
@@ -38,8 +38,54 @@ const person = {
   },
 };
 
-console.log(person.name);
-console.log(person.toString());
+// console.log(person.name);
+// console.log(person.toString());
 
-const numbers = [1, [2, 3, 4], "Abdulahi"];
-console.log(numbers[1][0]);
+const numbers = [1, [2, 3, 4], "Abdulahi", person];
+console.log(numbers[3].name);
+
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.push(100);
+numbers.unshift(99);
+
+console.log(numbers);
+
+numbers.pop();
+console.log("After pop", numbers);
+
+numbers.shift();
+console.log("After shift", numbers);
+
+const sliced = numbers.slice(2, 4);
+console.log(sliced);
+console.log(numbers);
+
+const spliced = numbers.splice(2, 1, 100, "Abdulahi");
+console.log("spliced", spliced);
+console.log("Numbers", numbers);
+
+*/
+
+const numbers = [1, 2, 3, 4, 5];
+
+// const sum = numbers.reduce((a, b) => a + b);
+// const even = numbers.filter((a) => a % 2 == 0);
+// console.log(even);
+
+function add(a, b) {
+  return a + b;
+}
+
+const add2 = function (a, b) {
+  return a + b;
+};
+
+function display(value) {
+  console.log(value);
+}
+
+//first class citizens
+
+console.log(add(1, 2));
+console.log(add2(1, 2));
