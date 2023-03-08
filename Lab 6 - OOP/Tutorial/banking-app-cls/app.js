1a2581fe2bb9cead07e513740990156bd7e04454
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-03-08 12:07:53 +0300

    updated classes

---
+++ Lab 6 - OOP/Tutorial/banking-app-cls/app.js
@@ -0,0 +1,5 @@
+import BankAccount from "./model/bank-account.js";
+
+const account = new BankAccount(1200)
+
+console.log(account.toString());
---
+++ Lab 6 - OOP/Tutorial/banking-app-cls/model/bank-account.js
@@ -0,0 +1,24 @@
+export default class BankAccount {
+    constructor(balance) {
+        this._accountNo = Math.floor(Math.random() * 1000)
+        this._balance = balance
+    }
+
+    getAccountNo() {
+        return this._accountNo
+    }
+    getBalance() {
+        return this._balance
+    }
+
+    deposit(amount) {
+        this._balance += amount
+    }
+    withdraw(amount) {
+        this._balance -= amount
+    }
+
+    toString() {
+        return ` Acc No : ${this._accountNo}, Balance : ${this._balance}`
+    }
+}
