af160a3f5f36bcd0d1f8327f15c59d4380084e32
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-03-08 10:00:30 +0300

    lab 6 initial

---
+++ Lab 6 - OOP/Tutorial/banking-app/bank.js
@@ -0,0 +1,11 @@
+// npm init
+// CRTL + J
+
+const accounts = [
+    { accountNo: 123, balance: 500, type: "Saving" },
+    { accountNo: 234, balance: 4000, type: "Current" },
+    { accountNo: 345, balance: 35000, type: "Current" },
+    { accountNo: 456, balance: 60000, type: "Saving" },
+]
+
+console.log(accounts);
