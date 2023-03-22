b7f717757d5c61e6c073ccf0f08862ac3d4e8c4b
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-03-22 12:05:03 +0300

    final code

--- Lab 7 Client JS/Tutorial/Country Explorer/js/index.js
+++ Lab 7 Client JS/Tutorial/Country Explorer/js/index.js
@@ -1,9 +1,9 @@
-// Links to the elements you want to interact with
+// // Links to the elements you want to interact with
 const regionDL = document.querySelector("#region")
 const countryDD = document.querySelector("#country")
 const factsArea = document.querySelector("#facts-area")
 
-// URLS
+// // URLS
 
 const regionURL = 'https://restcountries.com/v3.1/region/'
 const countryURL = 'https://restcountries.com/v3.1/name/'
@@ -18,6 +18,7 @@ async function handleRegionChange() {
     `).join(' ')
 
     countryDD.innerHTML = options
+    handleCountryChange()
 }
 
 async function handleCountryChange() {
@@ -30,10 +31,51 @@ async function handleCountryChange() {
 }
 
 function changeCountryToFacts(country) {
+    const currKey = Object.keys(country.currencies)[0]
+    const currency = country.currencies[currKey].name
+
+    const langKeys = Object.keys(country.languages)
+    const languages = langKeys
+        .map(key => country.languages[key]).join(' - ')
+
+    // 
     return `
         <h2>Facts About ${country.name.common}</h2>
         <img src="${country.flags.png}" alt="${country.flags.alt}">
-        <br>    
-        
+        <br>   
+        <table>
+            <tr>
+                <th>Official Name</th>
+                <td>${country.name.official}</td>
+            </tr>
+            <tr>
+                <th>Capital</th>
+                <td>${country.capital}</td>
+            </tr>
+            <tr>
+                <th>Currencies</th>
+                <td>${currency}</td>
+            </tr>
+            <tr>
+                <th>Population</th>
+                <td>${country.population}</td>
+            </tr>
+            <tr>
+                <th>Languages</th>
+                <td>${languages}</td>
+            </tr>
+
+        </table>
+
+
     `
 }
+// const person = {
+//     name: 'Ali',
+//     age: 90
+// }
+
+// const keys = Object.keys(person)
+// console.log(Object.values(person));
+// console.log(Object.key(person));
+// console.log(person[keys[0]]);
