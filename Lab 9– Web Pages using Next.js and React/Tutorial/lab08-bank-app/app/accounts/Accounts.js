4bb465df299ff4e25efbe094d29ee75939ae618a
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-04-12 11:44:07 +0300

    updated code for dropdown

--- Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/accounts/Accounts.js
+++ Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/accounts/Accounts.js
@@ -1,11 +1,16 @@
+'use client'
 import React from 'react'
 import styles from '../page.module.css'
 
 export default function Accounts({ initialAccounts }) {
+    function handleTypeChange(e) {
+        alert(e.target.value)
+    }
+
     return (
         <div>
             <label htmlFor="acctType" className={styles.label}>Account Type</label>
-            <select id="acctType" className={styles.dropdown}>
+            <select id="acctType" className={styles.dropdown} onChange={e => handleTypeChange(e)}>
                 <option value="All">All</option>
                 <option value="Saving">Saving</option>
                 <option value="Current">Current</option>
